7325c4b78123206adc9b3af280f9cb379fa5ee96
Thesohan <sohan.cse16@nituk.ac.in>
2018-08-12 02:12:34 +0530

    WEEKLY MENU LAYOUT IS CREATED BUT NOT COMPLETLY, FAQ fragment working fine,

--- app/src/main/java/drunkcoder/com/foodheaven/Fragments/FaqFragment.java
+++ app/src/main/java/drunkcoder/com/foodheaven/Fragments/FaqFragment.java
@@ -4,16 +4,73 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import drunkcoder.com.foodheaven.Adapters.CallForAssistanceArrayAdapter;
+import drunkcoder.com.foodheaven.Adapters.FaqArrayAdapter;
+import drunkcoder.com.foodheaven.Models.Assistance;
+import drunkcoder.com.foodheaven.Models.Faq;
+import drunkcoder.com.foodheaven.R;
 
 public class FaqFragment extends Fragment {
+    private ListView faqlistView;
+    private ArrayList<Faq> faqArrayList=new ArrayList<>();
+    private FaqArrayAdapter faqArrayAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view=inflater.inflate(R.layout.f_a_q_fragment,container,false);
+        faqlistView=view.findViewById(R.id.faqListView);
+        faqArrayAdapter=new FaqArrayAdapter(getContext(),faqArrayList);
+        faqlistView.setAdapter(faqArrayAdapter);
+        fetchFaqFromFirebase();
+
+        return view;
+    }
+
+    private void fetchFaqFromFirebase() {
+
+        FirebaseDatabase.getInstance().getReference("Faq").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                faqArrayList.add(dataSnapshot.getValue(Faq.class));
+                if (faqArrayAdapter != null)
+                    faqArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public FaqFragment() {
--- app/src/main/java/drunkcoder/com/foodheaven/Fragments/WeeklyMenuFragment.java
+++ app/src/main/java/drunkcoder/com/foodheaven/Fragments/WeeklyMenuFragment.java
@@ -1,22 +1,64 @@
 package drunkcoder.com.foodheaven.Fragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+
+import androidx.annotation.FontRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+import drunkcoder.com.foodheaven.R;
 
 public class WeeklyMenuFragment extends Fragment {
+    ImageView sunImageView,monImageView,tuesImageView,thrusImageView,wedImageView,friImageView,satImageView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.weekly_menu_fragment, container, false);
+        sunImageView=view.findViewWithTag("SUN");
+        monImageView=view.findViewWithTag("MON");
+        tuesImageView=view.findViewWithTag("TUES");
+        wedImageView=view.findViewWithTag("WED");
+        thrusImageView=view.findViewWithTag("THRUS");
+        friImageView=view.findViewWithTag("FRI");
+        satImageView=view.findViewWithTag("SAT");
+
+        setImageTextDrawable(sunImageView);
+        setImageTextDrawable(monImageView);
+        setImageTextDrawable(tuesImageView);
+        setImageTextDrawable(wedImageView);
+        setImageTextDrawable(thrusImageView);
+        setImageTextDrawable(friImageView);
+        setImageTextDrawable(satImageView);
+        return view;
     }
 
-    public WeeklyMenuFragment() {
+    private void setImageTextDrawable(ImageView imageView) {
+        Typeface custom_font=null;
+            custom_font=ResourcesCompat.getFont(getContext(),R.font.josefin_bold);
+        TextDrawable textDrawable=TextDrawable.builder()
+                .beginConfig().textColor(Color.BLACK)
+                .useFont(custom_font)
+                .fontSize(60).endConfig()
+                .buildRoundRect(imageView.getTag().toString(), Color.CYAN,10);
+
+        imageView.setImageDrawable(textDrawable);
+
+    }
+
+    void showTodaysMenu(View view){
+        Toast.makeText(getContext(), "hello", Toast.LENGTH_SHORT).show();
+
     }
 
     public static WeeklyMenuFragment newInstance() {
