31280868d8064d721c3e84b707f518fac1e5cbb9
arvind <negiarvind229@gmail.com>
2018-08-12 18:34:51 +0530

    Khuch Bhi nahi kia

--- app/src/main/java/drunkcoder/com/foodheaven/Fragments/WantsToEatFragment.java
+++ app/src/main/java/drunkcoder/com/foodheaven/Fragments/WantsToEatFragment.java
@@ -8,11 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -34,6 +41,8 @@ public class WantsToEatFragment extends Fragment {
     private FirebaseRecyclerAdapter<FoodMenu, WantsToEatViewHolder> wantsToEatFoodAdapter;
     private DatabaseReference wantsToEatDatabaseReference;
     private Button wantsSubmitButton;
+    private int maxLimit;
+    private ArrayList<String> foodChooseList;
 
     @Nullable
     @Override
@@ -41,6 +50,8 @@ public class WantsToEatFragment extends Fragment {
         View view=inflater.inflate(R.layout.wants_to_eat_layout,container,false);
         context=getContext();
         wantsToEatRecyclerView=view.findViewById(R.id.wantsToEatRecyclerView);
+        wantsToEatDatabaseReference=FirebaseDatabase.getInstance().getReference("FavouriteFood");
+        foodChooseList=new ArrayList<>();
         wantsSubmitButton=view.findViewById(R.id.wantsSubmitButton);
         layoutManager=new LinearLayoutManager(context);
         wantsToEatRecyclerView.setHasFixedSize(true);
@@ -49,6 +60,16 @@ public class WantsToEatFragment extends Fragment {
         wantsSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if(foodChooseList.size()!=0){
+                    Toast.makeText(context,"Submitted Succesfully",Toast.LENGTH_SHORT).show();
+                    //make the entry in firebase
+
+                    for(int i=0;i<foodChooseList.size();i++){
+
+                        //wantsToEatDatabaseReference.child("ChoosedBy").child(foodChooseList.get(i)).child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue()
+                    }
+                }
                 
             }
         });
@@ -57,24 +78,49 @@ public class WantsToEatFragment extends Fragment {
 
     private void loadWantToEatImages() {
 
-        wantsToEatDatabaseReference= FirebaseDatabase.getInstance().getReference("FavouriteFood").child("FoodImages");
-        
-        wantsToEatFoodAdapter=new FirebaseRecyclerAdapter<FoodMenu, WantsToEatViewHolder>(FoodMenu.class,
-                R.layout.wants_to_eat_raw_layout,WantsToEatViewHolder.class,wantsToEatDatabaseReference) {
+        DatabaseReference databaseReference=wantsToEatDatabaseReference.child("FoodImages");
+        wantsToEatDatabaseReference.child("maxLimit").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            protected void populateViewHolder(WantsToEatViewHolder wantsToEatViewHolder, FoodMenu FoodMenu, int i) {
-                wantsToEatViewHolder.wantsFoodNameTextView.setText(FoodMenu.getFoodName());
-                wantsToEatViewHolder.wantsFoodDescriptionTextView.setText(FoodMenu.getFoodDescription());
-                Picasso.with(context).load(FoodMenu.getImageUrl()).into(wantsToEatViewHolder.wantsFoodImageView);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot!=null){
+                    maxLimit=dataSnapshot.getValue(Integer.class);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        wantsToEatFoodAdapter=new FirebaseRecyclerAdapter<FoodMenu, WantsToEatViewHolder>(FoodMenu.class,
+                R.layout.wants_to_eat_raw_layout,WantsToEatViewHolder.class,databaseReference) {
+            @Override
+            protected void populateViewHolder(WantsToEatViewHolder wantsToEatViewHolder, final FoodMenu foodMenu, int i) {
+                wantsToEatViewHolder.wantsFoodNameTextView.setText(foodMenu.getFoodName());
+                wantsToEatViewHolder.wantsFoodDescriptionTextView.setText(foodMenu.getFoodDescription());
+                Picasso.with(context).load(foodMenu.getImageUrl()).into(wantsToEatViewHolder.wantsFoodImageView);
                 
                 wantsToEatViewHolder.wantsFoodCheckBox.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         CheckBox checkBox=(CheckBox)view;
                         if(checkBox.isChecked()){
-                            
+                            if(foodChooseList.size()<maxLimit){
+                                foodChooseList.add(foodMenu.getFoodName());
+                            }else{
+                                checkBox.setChecked(false);
+                                Toast.makeText(context,"You can only select "+maxLimit+" items",Toast.LENGTH_LONG).show();
+                            }
                         }else{
-                            
+                            Log.i("removed","------"+foodMenu.getFoodName());
+                            for(int j=0;j<foodChooseList.size();j++){
+                                if(foodChooseList.get(j).equals(foodMenu.getFoodName())){
+                                    Log.i("inside if",foodChooseList.get(j));
+                                    foodChooseList.remove(j);
+                                    break;
+                                }
+                            }
                         }
                     }
                 });
