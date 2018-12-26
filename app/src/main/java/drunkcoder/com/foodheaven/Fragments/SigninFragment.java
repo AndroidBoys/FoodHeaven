b294d399915922ae02a8589e8b1d22895c8875d5
Thesohan <sohan.cse16@nituk.ac>
2018-12-26 19:45:26 +0530

    final commit

--- app/src/main/java/drunkcoder/com/foodheaven/Fragments/SigninFragment.java
+++ app/src/main/java/drunkcoder/com/foodheaven/Fragments/SigninFragment.java
@@ -2,6 +2,8 @@ package drunkcoder.com.foodheaven.Fragments;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -51,6 +54,8 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
     private boolean isPhoneNumber;
     private boolean isEmail;
     private String username="Sample@test.com";
+    private boolean passwordVisible=false;
+    private ImageView passwordVisibilityImageView;
 
     public static SigninFragment newInstance() {
 
@@ -68,12 +73,14 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_signin,container,false);
         hostingActivity = (AuthenticationActivity) getActivity();
 
+        passwordVisibilityImageView=view.findViewById(R.id.passwordVisibilityImageView);
         usernameEditText = view.findViewById(R.id.usernameEdittext);
         passwordEditText = view.findViewById(R.id.passwordEdittext);
         signinButton = view.findViewById(R.id.signinButton);
         signupTextview = view.findViewById(R.id.signupTextview);
         forgotTextview = view.findViewById(R.id.forgotTextview);
 
+        passwordVisibilityImageView.setOnClickListener(this);
         signupTextview.setOnClickListener(this);
         signinButton.setOnClickListener(this);
         forgotTextview.setOnClickListener(this);
@@ -98,6 +105,26 @@ public class SigninFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.forgotTextview:
                 recoverPassword();
+                break;
+            case R.id.passwordVisibilityImageView:
+
+                //saving cursor's positions
+                int  start =passwordEditText.getSelectionStart();
+                int end=passwordEditText.getSelectionEnd();
+
+                if(passwordVisible){
+                    passwordVisible=false;
+                    passwordVisibilityImageView.setImageResource(R.drawable.ic_visibility_off_black_24dp);
+                    passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
+                }
+                else{
+                    passwordVisible=true;
+                    passwordEditText.setTransformationMethod(null);
+                    passwordVisibilityImageView.setImageResource(R.drawable.ic_visibility_black_24dp);
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+                passwordEditText.setSelection(start, end);
+                break;
 
         }
     }
